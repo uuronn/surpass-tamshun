36eda0babe825519cd443f915432023c6032c73e
鶴盛士 <139316128+tt1125@users.noreply.github.com>
2024-09-23 08:40:32 +0900

    Top画面のみ

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,5 +1,14 @@
-import Login from "@/components/login/Login";
-
 export default function Home() {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        backgroundImage: `url('https://gakaisozai.up.seesaa.net/01247252N000000003/151351867304205811177_BG560a_1280.jpg')`,
+        backgroundSize: "cover",
+      }}
+    >
+      Top
+    </div>
+  );
 }
--- front/src/context/AuthContext.tsx
+++ front/src/context/AuthContext.tsx
@@ -36,7 +36,8 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     email: "",
     name: "",
   }); //nullの時は未ログイン、undefinedの時はローディング中、userの時はログイン中
-  const fetchUser = async () => {};
+  useEffect(() => {}, []); //ログイン状態を確認する処理
+  const fetchUser = async () => {}; //ユーザー情報を取得する処理
   return (
     <AuthContext.Provider value={{ user, fetchUser }}>
       {user ? (

cf38fa8b36906571a5bac2e3f8a0f5db733ff885
uuronn <uuronprogram@gmail.com>
2024-09-25 18:25:22 +0900

    対戦できるようにapi修正

--- back/database/migrations/2024_09_24_180358_create_room_table.php
+++ back/database/migrations/2024_09_24_180358_create_room_table.php
@@ -15,17 +15,22 @@ return new class extends Migration
             $table->uuid('id')->primary();
 
             $table->uuid('host_user_id');
+            $table->integer('host_user_hit_point');
             $table->integer('host_user_attack_power');
             $table->integer('host_user_guard_power');
-            $table->integer('host_user_hit_point');
+            $table->integer('host_user_speed_power');
             $table->json('host_user_skills')->nullable();
 
             $table->uuid('join_user_id')->nullable();
+            $table->integer('join_user_hit_point')->nullable();
             $table->integer('join_user_attack_power')->nullable();
             $table->integer('join_user_guard_power')->nullable();
-            $table->integer('join_user_hit_point')->nullable();
+            $table->integer('join_user_speed_power')->nullable();
             $table->json('join_user_skills')->nullable();
-//
+
+            $table->uuid('currentTurnUser')->nullable();
+
+            $table->boolean('is_connect')->default(false);
             $table->boolean('is_battle_finish')->default(false);
             $table->timestamps();
         });
